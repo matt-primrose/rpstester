e6173b9e3a7d31bbc9107bd5c2dbd00c42b8c8f7
mprimros <matt.c.primrose@intel.com>
2019-08-27 17:04:33 -0700

    startTest function currently not working

--- rcstester.js
+++ rcstester.js
@@ -22,8 +22,10 @@ limitations under the License.
 
 'use strict';
 const fs = require('fs');
+const crypto = require('crypto');
 const rcsTesterVersion = '0.0.1';
 const settings = new Object();
+var emulatedClients;
 // Execute based on incoming arguments
 function run(argv) {
     let args = parseArguments(argv);
@@ -48,44 +50,93 @@ function consoleHelp(){
 }
 
 function startTest(){
+    emulatedClients = new Object();
     let testfile = JSON.parse(fs.readFileSync(__dirname + '/testmessages.json', 'utf8'));
-    connectToServer(testfile.messages[0]);
+    for (let x = 0; x < settings.num; x++){
+        let test = new Object();
+        test = testfile.messages[1];
+        test.nonce = generateFWNonce();
+        //console.log(test.nonce);
+        test.uuid = generateUuid();
+        //console.log(test.uuid);
+        //console.log(test);
+        emulatedClients[test.uuid] = test;
+        //console.log(emulatedClients[x]);
+    }
+    console.log(emulatedClients);
+    for (let x in emulatedClients){
+        connectToServer(emulatedClients[x], function(resp){
+            let guidCheck = false;
+            if (emulatedClients[x].uuid == resp.uuid) { guidCheck = true; }
+            console.log('Client UUID: ' + resp.uuid + '\n\rStatus: '+ resp.status + '\n\rAction: ' + resp.action + '\n\rRCS Nonce: ' + resp.nonce + '\n\rNonce Verification: '+ nonceCheck(resp.nonce, resp.signature) + '\n\rGuid Check: ' + guidCheck);
+        });
+    }
 }
 
-function connectToServer(message){
+function nonceCheck(nonce, signature){
+    // Need to verify signature, verify the fwNonce matches what was sent and verify the mcNonce matches what is in the signature.
+    return true;
+}
+
+function connectToServer(message, callback){
     let WebSocket = require('ws');
     let ws = new WebSocket(settings.url);
     ws.on('open', function(){
-        ws.send(JSON.stringify(message));
+        // for (let x = 0; x < emulatedClients.length; x++){
+        //     if (message.uuid == emulatedClients[x].uuid){
+        //         emulatedClients[x].tunnel = ws;
+        //         emulatedClients[x].tunnel.send(JSON.stringify(message));
+        //     }
+        // }
     });
     ws.on('message', function(data){
-        console.log('message received');
         let cmd = null;
         try {
             cmd = JSON.parse(data);
         } catch(ex){
             console.log('Unable to parse server response: ' + data);
         }
-        if (typeof cmd != 'object') { console.log('Invalid server response: ' + cmd); }
+        if (typeof cmd != 'object') { console.log('Invalid server response: ' + data); }
         if (typeof cmd.errorText == 'string') { console.log('Server error: ' + cmd.errorText); }
         switch (cmd.action){
             case 'acmactivate': {
-                console.log('acmactivate response: ' + JSON.stringify(cmd));
-                ws.close();
-                break;
+                // if (callback) { callback(cmd); }
+                // for (let x = 0; x < emulatedClients.length; x++){
+                //     if (cmd.uuid == emulatedClients[x].uuid){
+                //         emulatedClients[x].tunnel.close();
+                //     }
+                // }
+                // break;
             }
             case 'ccmactivate': {
-                console.log('ccmactivate response: ' + JSON.stringify(cmd));
-                ws.close();
-                break;
+                // if (callback) { callback(cmd); }
+                // for (let x = 0; x < emulatedClients.length; x++){
+                //     if (cmd.uuid == emulatedClients[x].uuid){
+                //         emulatedClients[x].tunnel.close();
+                //     }
+                // }
+                // break;
             }
             default: {
-                console.log('Invalid server response, command: ' + cmd.action);
-                ws.close();
+                if (cmd.action) { console.log('Invalid server response, command: ' + cmd.action); }
+                if (callback) { callback(cmd); }
+                for (let x = 0; x < emulatedClients.length; x++){
+                    if (cmd.uuid == emulatedClients[x].uuid){
+                        emulatedClients[x].tunnel.close();
+                    }
+                }
                 break;
             }
         }
     });
+}
+
+function generateFWNonce(){
+    return crypto.randomBytes(20); 
+}
+
+function generateUuid(){
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.randomFillSync(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 }
 
 // Figure out if any arguments were provided, otherwise show help
